414c59d3aa50df5f67b2c2c5821772eac14aec31
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-08-09 17:18:25 +0000

    reset demo script

--- genaisrc/poem.genai.mjs
+++ genaisrc/poem.genai.mjs
@@ -1,9 +1,1 @@
-script({
-    files: "bicep/main.bicep"
-})
-const file = def("FILE", env.files)
-$`Analyze ${file} and report errors.
-
-- generate a diagram of all resources using mermaid
-- validate best practices for Azure Bicep files
-`
+$`Write a poem.`
