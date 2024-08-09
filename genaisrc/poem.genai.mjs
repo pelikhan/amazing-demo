5ec27b92eac5fc99a62a175b29dffeb1b5ac360e
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-08-09 17:17:55 +0000

    adding a few demo files

--- genaisrc/poem.genai.mjs
+++ genaisrc/poem.genai.mjs
@@ -4,5 +4,6 @@ script({
 const file = def("FILE", env.files)
 $`Analyze ${file} and report errors.
 
+- generate a diagram of all resources using mermaid
 - validate best practices for Azure Bicep files
 `
