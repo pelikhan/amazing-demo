9576d3d844c76f3ef65f0631266126647aa260b4
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-01-15 23:48:05 +0000

    revert to simple poem

--- genaisrc/poem.genai.mjs
+++ genaisrc/poem.genai.mjs
@@ -1,5 +1,1 @@
-script({ files: "webapp/main.bicep"})
-def("FILE", env.files)
-$`you are an expert in Azure bicep. Find and report all the errors in the FILE.
-Use the annotation format.
-.`
+$`write a poem in code`
