510e94f1e4957d84adc4f715cd9a30e19d7ca5c6
pelikhan <jhalleux@microsoft.com>
2025-01-21 10:55:53 -0800

    added script

--- genaisrc/poem.genai.mjs
+++ genaisrc/poem.genai.mjs
@@ -1,1 +1,9 @@
-$`write a poem in code`
+script({
+    files: "webapp/main.bicep"
+})
+const files = env.files
+def("FILES", files, { endsWith: ".bicep"})
+$`You are an expert at Azure Bicep.
+Analyze the content of the file <FILES> 
+and report errors and warnings.`
+
