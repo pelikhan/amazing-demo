c030e4b3086c3197ae4883f0a88358f946dab358
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-03-12 15:15:22 +0000

    mcp files

--- genaisrc/poem.genai.mjs
+++ genaisrc/poem.genai.mjs
@@ -1,1 +1,25 @@
-$`write a poem in emojis`
+script({
+  files: "webapp/main.bicep",
+});
+
+const { text: summary } = await runPrompt(
+  (ctx) => {
+    ctx.def("FILE", env.files, { endsWith: ".bicep" });
+    ctx.$`Sumarize FILE`;
+  },
+  { model: "small" }
+);
+
+// analyze
+const res = await runPrompt(
+  (ctx) => {
+    ctx.def("FILE", env.files, { endsWith: ".bicep" });
+    ctx.$`You are a Azure Bicep expert following best practices.
+Analyze and report errors in <FILE>.
+${summary}
+Use annotation format. Fix the errors in the files.`;
+  },
+  { applyEdits: true, model: "large" }
+);
+
+// push branch
